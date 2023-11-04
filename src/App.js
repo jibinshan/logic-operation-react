7a4b1790956e5aa3586a2e6633c1d38dfdca9e90
jibinshan <jibinshanu23@gmail.com>
2023-11-04 15:41:35 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,132 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+export const App = ()=> {
+  const [id,setId] = useState([])
+  const [numbers, setNumbers] = useState([
+    {
+      id: 1,
+      num: "1",
+      status: false,
+    },
+    {
+      id: 2,
+      num: "2",
+      status: false,
+    },
+    {
+      id: 3,
+      num: "3",
+      status: false,
+    },
+    {
+      id: 4,
+      num: "4",
+      status: false,
+    },
+  ]);
+  
+const handlecheckone = (e,id)=>{
+   
+     console.log(id);
+     if (e.target.checked) {
+    
+       
+       setId((prev)=>{
+        return [
+          ...prev,
+           id
+        ]
+       }) 
+     }
+   
+}
+const todivtwo = ()=>{
+    let arr= numbers.map((number)=>{
+      console.log(id);
+      id.filter((ids)=>{
 
-function App() {
+        if (number.id === ids) {
+          number.status = true
+        }
+        return null
+      })
+      return number
+     })
+  setNumbers(arr)
+
+}
+const handlecheckthree =(e,id)=>{
+  setId([])
+  if (e.target.value) {
+    
+    setId((prev)=>{
+     return [
+       ...prev,
+       id
+    ]
+   }
+    )
+  }
+
+ 
+}
+
+const todivone = ()=>{
+ const array = numbers.map((num)=>{
+    id.filter((ids)=>{
+      if (num.id === ids) {
+
+        num.status = false
+      } 
+      return null
+    })
+    return num
+  })
+  setNumbers(array)
+
+}
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="main-div">
+          <div className="div-one">
+            {
+              numbers.map((number)=>{
+                if (number.status !== true) {
+                  
+                  return(
+                    <div className="check" key={number.id}>
+                    <input type="checkbox"  onChange={(e)=>handlecheckone(e,number.id)}/>
+                    <h3>{number.num}</h3>
+                    </div>
+                  )
+                }
+                return null
+              })
+            }
+          </div>
+          <div className="div-two">
+            <button onClick={todivtwo}> {">"} </button>
+            <button onClick={todivone}> {"<"} </button>
+          </div>
+          <div className="div-three">
+            {
+              numbers.map((number)=>{
+                if(number.status === true){
+
+                  return(
+                    <div className="check" key={number.id}>
+                  <input type="checkbox" onChange={(e)=>handlecheckthree(e,number.id)}/>
+                  <h3>{number.num}</h3>
+                  </div>
+                )
+              }
+              return null
+              })
+            }
+          </div>
+        </div>
     </div>
   );
 }
 
-export default App;
+
